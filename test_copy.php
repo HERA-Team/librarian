99728c1e4d933877fcc4c4d5f0081cbc79cc4850
David Anderson <davea@ssl.berkeley.edu>
2015-11-18 09:41:12 -0800

    tweak

--- test_copy.php
+++ test_copy.php
@@ -5,7 +5,7 @@ require_once("hl_rpc_client.php");
 
 $ret = create_copy_task(
     TASK_TYPE_PUSH,
-    "test_site", "Store 0", "file_1_0", "test_site", "Store 1", false
+    "Karoo", "Store 0", "file_1_0", "UCB_test", "Store 0", false
 );
 
 if (!$ret->success) {
