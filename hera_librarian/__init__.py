802374da83656d781e47d279121ac729ab3b1a0d
Peter Williams <peter@newton.cx>
2016-02-09 15:51:14 -0500

    hera_librarian: reorganize the code into a class
    The LibrarianClient class loads up the configuration information for a
    particular Librarian site, and interactions with that librarian go through the
    client object.

--- hera_librarian/__init__.py
+++ hera_librarian/__init__.py
@@ -18,109 +18,87 @@ def get_client_config():
     return json.loads(s)
 
 
-def get_site(site_name):
-    config = get_client_config()
-    site = config['sites'].get (site_name)
-    if site is None:
-        raise NoSuchSiteError (site_name)
-    return site
+class LibrarianClient (object):
+    site_name = None
+    "The name of the Librarian site we target."
+
+    config = None
+    "The JSON config fragment corresponding to the desired site."
+
+    def __init__ (self, site_name):
+        self.site_name = site_name
+
+        config = get_client_config ()
+        self.config = config['sites'].get (site_name)
+        if self.config is None:
+            raise NoSuchSiteError (site_name)
 
 
-def _do_http_post(req, site):
-    """do a POST operation, passing a JSON version of the request and expecting a
-    JSON reply; return the decoded version of the latter.
+    def _do_http_post(self, operation, **kwargs):
+        """do a POST operation, passing a JSON version of the request and expecting a
+        JSON reply; return the decoded version of the latter.
 
-    """
-    req_json = json.dumps(req)
-    params = urllib.urlencode({'request': req_json})
-    url = site['url']+'/hl_rpc_handler.php'
-    f = urllib.urlopen(url , params);
-    reply_json = f.read()
-    try:
-        reply = json.loads(reply_json)
-    except ValueError:
-        print('failed to parse reply as JSON: ' + reply_json)
-        raise
-    return reply
+        """
+        kwargs['operation'] = operation
+        kwargs['authenticator'] = self.config['authenticator']
+        req_json = json.dumps(kwargs)
 
-
-# RPC calls
-
-def create_observation(site_name, obs_id, julian_date, polarization, length):
-    site = get_site(site_name)
-
-    req = {
-        'operation': 'create_observation',
-        'authenticator': site['authenticator'],
-        'id': obs_id,
-        'julian_date': julian_date,
-        'polarization': polarization,
-        'length': length,
-    }
-    return _do_http_post(req, site)
+        params = urllib.urlencode({'request': req_json})
+        url = self.config['url'] + '/hl_rpc_handler.php'
+        f = urllib.urlopen(url, params);
+        reply_json = f.read()
+        try:
+            reply = json.loads(reply_json)
+        except ValueError:
+            print('failed to parse reply as JSON: ' + reply_json)
+            raise
+        return reply
 
 
-def create_file(site_name, store_name, file_name, type, obs_id, size, md5):
-    site = get_site(site_name)
-
-    req = {
-        'operation': 'create_file',
-        'authenticator': site['authenticator'],
-        'store_name': store_name,
-        'file_name': file_name,
-        'type': type,
-        'obs_id': obs_id,
-        'size': size,
-        'md5': md5,
-    }
-    return _do_http_post(req, site)
+    def create_observation(self, obs_id, julian_date, polarization, length):
+        return self._do_http_post ('create_observation',
+            id=obs_id,
+            julian_date=julian_date,
+            polarization=polarization,
+            length=length,
+        )
 
 
-def delete_file(site_name, file_name, store_name):
-    site = get_site(site_name)
-
-    req = {
-        'operation': 'delete_file',
-        'authenticator': site['authenticator'],
-        'name': file_name,
-        'store_name': store_name,
-    }
-    return _do_http_post(req, site)
-
-
-def get_store_list(site_name):
-    site = get_site(site_name)
-
-    req = {
-        'operation': 'get_store_list',
-        'authenticator': site['authenticator'],
-    }
-    return _do_http_post(req, site)
+    def create_file(self, store_name, file_name, type, obs_id, size, md5):
+        return self._do_http_post ('create_file',
+            store_name=store_name,
+            file_name=file_name,
+            type=type,
+            obs_id=obs_id,
+            size=size,
+            md5=md5,
+        )
 
 
-def recommended_store(site_name, file_size):
-    site = get_site(site_name)
-
-    req = {
-        'operation': 'recommended_store',
-        'authenticator': site['authenticator'],
-        'file_size': file_size,
-    }
-    return _do_http_post(req, site)
+    def delete_file(self, file_name, store_name):
+        return self._do_http_post ('delete_file',
+            name=file_name,
+            store_name=store_name,
+        )
 
 
-def create_copy_task(task_type, local_site_name, local_store_name, file_name,
-                     remote_site_name, remote_store_name, delete_when_done):
-    site = get_site(local_site_name)
+    def get_store_list(self):
+        return self._do_http_post ('get_store_list')
 
-    req = {
-        'operation': 'create_copy_task',
-        'authenticator': site['authenticator'],
-        'task_type': task_type,
-        'local_store_name': local_store_name,
-        'file_name': file_name,
-        'remote_site_name': remote_site_name,
-        'remote_store_name': remote_store_name,
-        'delete_when_done': delete_when_done,
-    }
-    return _do_http_post(req, site)
+
+    def recommended_store(self, file_size):
+        return self._do_http_post ('recommended_store',
+            file_size=file_size
+        )
+
+
+    def create_copy_task (self, task_type, local_store_name, file_name,
+                          remote_site_name, remote_store_name, delete_when_done):
+        return self._do_http_post ('create_copy_task',
+            task_type=task_type,
+            local_store_name=local_store_name,
+            file_name=file_name,
+            remote_site_name=remote_site_name,
+            remote_store_name=remote_store_name,
+            delete_when_done=delete_when_done,
+        )
