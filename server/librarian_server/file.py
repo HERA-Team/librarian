cc8c7e5331d39fa0d2a59b46cb9c19d73aea47fa
HERA Observer <obs@qmaster>
2016-12-08 22:29:05 +0200

    server: only require LST if we're creating an Observation for this File
    This allows us to create Files associated with preexisting observations when the
    file doesn't have all of the metadata that we want for an Observation record.
    For instance, a bad_ants file is not MIRIAD and therefore we can't infer its LST,
    but it is always created after the original MIRIAD file, so that's OK.

--- server/librarian_server/file.py
+++ server/librarian_server/file.py
@@ -121,7 +121,6 @@ class File (db.Model):
         size = required_arg (info, int, 'size')
         md5 = required_arg (info, unicode, 'md5')
         type = required_arg (info, unicode, 'type')
-        lst = required_arg (info, float, 'lst')
 
         from .observation import Observation
         obsid = required_arg (info, int, 'obsid')
@@ -129,6 +128,7 @@ class File (db.Model):
 
         if obs is None:
             start_jd = required_arg (info, float, 'start_jd')
+            lst = required_arg (info, float, 'lst')
             db.session.add (Observation (obsid, start_jd, None, lst))
 
         inst = File (name, type, obsid, source_name, size, md5)
