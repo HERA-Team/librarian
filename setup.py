ef3b403d39ace09f037291e79b27c3cdfa47c3f0
Peter Williams <peter@newton.cx>
2018-08-14 09:27:37 -0400

    Version 0.1.5
    Now pip-able, hopefully.

--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
-from distutils.core import setup
+from setuptools import setup
 
-__version__ = '0.1.4.99'
+__version__ = '0.1.5'
 
-setup_args = {
-    'name': 'hera_librarian',
-    'author': 'HERA Team',
-    'license': 'BSD',
-    'packages': ['hera_librarian'],
-    'scripts': [
+
+setup(
+    name='hera_librarian',
+    version=__version__,
+    author='HERA Team',
+    author_email='hera@lists.berkeley.edu',
+    url='https://github.com/HERA-Team/librarian/',
+    license='BSD',
+    description='A client for the HERA Librarian data management system',
+    long_description='''\
+The HERA Librarian is a distributed system for managing HERA collaboration
+data products. This package provides client libraries that allow you to
+communicate with a Librarian server. It also includes the server code,
+although those modules are not installed in a standard ``pip install``.
+
+The Librarian client and server currently only run on Python 2.
+''',
+
+    # These are the requirements for the *client library* only -- much lighter
+    # than for the server.
+    install_requires=[
+        'astropy >=2.0',
+    ],
+
+    packages=[
+        'hera_librarian',
+    ],
+
+    scripts=[
         'scripts/add_librarian_file_event.py',
         'scripts/add_obs_librarian.py',
         'scripts/launch_librarian_copy.py',
@@ -21,8 +44,11 @@ setup_args = {
         'scripts/librarian_stream_file_or_directory.sh',
         'scripts/upload_to_librarian.py',
     ],
-    'version': __version__
-}
 
-if __name__ == '__main__':
-    apply(setup, (), setup_args)
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering :: Astronomy',
+    ],
+)
