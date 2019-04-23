52500499df2a41bf7659f800fc78fd9926f97c9f
Paul La Plante <plaplant@sas.upenn.edu>
2019-04-22 17:55:13 -0700

    Make even more tweaks

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -1,6 +1,7 @@
 set -xe
 
-apt-get update; apt-get install -y gcc g++
+# get conda set up
+apt-get update; apt-get install -y gcc g++ openssh-server
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda config --add channels conda-forge
@@ -14,3 +15,17 @@ conda list -n ${ENV_NAME}
 
 # install other dependencies with pip
 pip install pytest-datafiles pytest-cov
+
+# export environment variables
+# python won't expand the tilde (~) character, so we have to do it ourselves
+echo 'export LIBRARIAN_CONFIG_PATH=`eval echo ${CIRCLE_WORKING_DIRECTORY}/ci/server-config-ci.json`' >> $BASH_ENV
+
+# set up librarian database
+cd server
+alembic upgrade head
+
+# set up ssh server
+/etc/init.d/ssh start
+
+# add localhost to known_hosts
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
