497fff286d589053b0eb555445a4d1a7722712d1
Paul La Plante <plaplant@sas.upenn.edu>
2019-09-19 08:15:22 -0400

    Parameterize temporary directories in tests; adjust CI settings

--- hera_librarian/tests/test_base_store.py
+++ hera_librarian/tests/test_base_store.py
@@ -9,7 +9,9 @@
 from __future__ import print_function, division, absolute_import
 import pytest
 import os
+import tempfile
 import json
+import shutil
 from hera_librarian import base_store, RPCError
 
 from . import ALL_FILES, filetypes, obsids, md5sums, pathsizes
@@ -17,27 +19,35 @@ from . import ALL_FILES, filetypes, obsids, md5sums, pathsizes
 
 @pytest.fixture
 def local_store():
-    return base_store.BaseStore("local_store", "/tmp", "localhost")
+    tempdir = tempfile.mkdtemp(dir="/tmp")
+    return base_store.BaseStore("local_store", tempdir, "localhost"), tempdir
 
 
 def test_path(local_store):
     # test that store path is prepended
-    assert local_store._path("my_dir") == "/tmp/my_dir"
+    dirpath = os.path.join(local_store[1], "my_dir")
+    assert local_store[0]._path("my_dir") == dirpath
 
     # test passing in an absolute path
     with pytest.raises(ValueError):
-        local_store._path("/tmp/my_dir")
+        local_store[0]._path("/tmp/my_dir")
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
 
 def test_ssh_slurp(local_store):
     # test simple command
-    assert local_store._ssh_slurp("echo hello world") == "hello world\n"
+    assert local_store[0]._ssh_slurp("echo hello world") == "hello world\n"
 
     # try a bogus command
     with pytest.raises(RPCError):
-        local_store._ssh_slurp("foo")
+        local_store[0]._ssh_slurp("foo")
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
@@ -49,88 +59,109 @@ def test_copy_to_store(tmpdir, local_store):
         print("hello world", file=f)
 
     # copy it over
-    local_store.copy_to_store(str(tmpdir), "test_directory")
+    local_store[0].copy_to_store(str(tmpdir), "test_directory")
 
     # check that it exists
-    assert local_store._ssh_slurp("ls /tmp/test_directory") == "my_file.txt\n"
+    dirpath = os.path.join(local_store[1], "test_directory")
+    assert local_store[0]._ssh_slurp("ls {}".format(dirpath)) == "my_file.txt\n"
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
 
 def test_chmod(local_store):
     # make a small test file on the store, then change permissions
-    local_store._ssh_slurp("touch /tmp/my_empty_file")
-    local_store._chmod("my_empty_file", "664")
+    tempdir = local_store[1]
+    temppath = os.path.join(tempdir, "my_empty_file")
+    local_store[0]._ssh_slurp("touch {}".format(temppath))
+    local_store[0]._chmod("my_empty_file", "664")
 
     # make sure permissions are correct
-    output = local_store._ssh_slurp("ls -l /tmp/my_empty_file")
+    output = local_store[0]._ssh_slurp("ls -l {}".format(temppath))
     perms = output.split(" ")[0]
     assert perms == "-rw-rw-r--"
+
+    # clean up
+    shutil.rmtree(os.path.join("/tmp", local_store[1]))
 
     return
 
 
 def test_move(local_store):
     # test moving a file
-    # first make sure that target file does not exist
-    local_store._ssh_slurp("rm -f /tmp/my_moved_file")
-    local_store._ssh_slurp("touch /tmp/my_empty_file")
-    local_store._move("my_empty_file", "my_moved_file")
-    assert local_store._ssh_slurp("ls /tmp/my_moved_file") == "/tmp/my_moved_file\n"
+    temppath = os.path.join(local_store[1], "my_empty_file")
+    local_store[0]._ssh_slurp("touch {}".format(temppath))
+    local_store[0]._move("my_empty_file", "my_moved_file")
+    temppath2 = os.path.join("/tmp", local_store[1], "my_moved_file")
+    assert local_store[0]._ssh_slurp("ls {}".format(temppath2)) == "{}\n".format(temppath2)
 
     # test trying to overwrite a file that already exists
     with pytest.raises(RPCError):
-        local_store._ssh_slurp("touch /tmp/my_empty_file")
-        local_store._move("my_empty_file", "my_moved_file")
+        local_store[0]._ssh_slurp("touch {}".format(temppath))
+        local_store[0]._move("my_empty_file", "my_moved_file")
 
     # remove existing files; test using chmod
-    local_store._ssh_slurp("rm -f /tmp/my_empty_file /tmp/my_moved_file")
-    local_store._ssh_slurp("touch /tmp/my_empty_file")
-    local_store._move("my_empty_file", "my_moved_file", chmod_spec=664)
-    output = local_store._ssh_slurp("ls -l /tmp/my_moved_file")
+    local_store[0]._ssh_slurp("rm -f {} {}".format(temppath, temppath2))
+    local_store[0]._ssh_slurp("touch {}".format(temppath))
+    local_store[0]._move("my_empty_file", "my_moved_file", chmod_spec=664)
+    output = local_store[0]._ssh_slurp("ls -l {}".format(temppath2))
     perms = output.split(" ")[0]
     assert perms == "-rw-rw-r--"
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
 
 def test_delete(local_store):
     # test removing a file
-    local_store._ssh_slurp("touch /tmp/my_empty_file")
-    local_store._delete("my_empty_file")
+    temppath = os.path.join(local_store[1], "my_empty_file")
+    local_store[0]._ssh_slurp("touch {}".format(temppath))
+    local_store[0]._delete("my_empty_file")
     assert (
-        local_store._ssh_slurp(
-            "if [ -f /tmp/my_empty_file ]; then echo file_still_exists; fi"
+        local_store[0]._ssh_slurp(
+            "if [ -f {} ]; then echo file_still_exists; fi".format(temppath)
         )
         == ""
     )
 
     # test deleting a write-protected file
-    local_store._ssh_slurp("mkdir /tmp/my_empty_dir; chmod 755 /tmp/my_empty_dir")
-    local_store._delete("my_empty_dir", chmod_before=True)
+    tempdir = os.path.join(local_store[1], "my_empty_dir")
+    local_store[0]._ssh_slurp("mkdir {0}; chmod 755 {0}".format(tempdir))
+    local_store[0]._delete("my_empty_dir", chmod_before=True)
     assert (
-        local_store._ssh_slurp(
-            "if [ -d /tmp/my_empty_dir ]; then echo dir_still_exists; fi"
+        local_store[0]._ssh_slurp(
+            "if [ -d {} ]; then echo dir_still_exists; fi".format(tempdir)
         )
         == ""
     )
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
 
 def test_create_tempdir(local_store):
     # make sure no temp dirs currently exist on host
-    local_store._ssh_slurp("rm -rf /tmp/libtmp.*")
-    tmppath = local_store._create_tempdir()
+    tempdir = os.path.join(local_store[1])
+    local_store[0]._ssh_slurp("rm -rf {}/libtmp.*".format(tempdir))
+    tmppath = local_store[0]._create_tempdir()
     # we don't know exactly what the directory name will be, because a random
     # 6-digit string is appended to the end
     assert tmppath.startswith("libtmp.")
     assert len(tmppath) == len("libtmp.") + 6
     # make sure it exists on the host
     assert (
-        local_store._ssh_slurp("ls -d1 /tmp/{}".format(tmppath))
-        == "/tmp/{}\n".format(tmppath)
+        local_store[0]._ssh_slurp("ls -d1 {}/{}".format(tempdir, tmppath))
+        == "{}/{}\n".format(tempdir, tmppath)
     )
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
 
@@ -140,10 +171,10 @@ def test_get_info_for_path(local_store, datafiles):
     # copy a datafile to store directory, so we can get its info
     filepaths = sorted(list(map(str, datafiles.listdir())))
     filename = os.path.basename(filepaths[0])
-    local_store.copy_to_store(filepaths[0], filename)
+    local_store[0].copy_to_store(filepaths[0], filename)
 
     # get the file info and check that it's right
-    info = local_store.get_info_for_path(filename)
+    info = local_store[0].get_info_for_path(filename)
     # make a dict of the correct answers
     # the uvh5 properties are first in these lists
     correct_dict = {
@@ -154,27 +185,33 @@ def test_get_info_for_path(local_store, datafiles):
     }
     assert info == correct_dict
 
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
+
     return
 
 
 def test_get_space_info(local_store):
     # get the disk information of the store
-    info = local_store.get_space_info()
+    info = local_store[0].get_space_info()
     assert "used" in info.keys()
     assert "available" in info.keys()
     assert "total" in info.keys()
     assert info["used"] + info["available"] == info["total"]
 
     # test using the cache -- make sure the info is the same
-    info_cached = local_store.get_space_info()
+    info_cached = local_store[0].get_space_info()
     assert info == info_cached
 
     # we also test the capacity, space_left, and usage_percentage properties
-    capacity = local_store.capacity
-    space_left = local_store.space_left
-    usage_percentage = local_store.usage_percentage
+    capacity = local_store[0].capacity
+    space_left = local_store[0].space_left
+    usage_percentage = local_store[0].usage_percentage
     assert capacity == info["total"]
     assert space_left == info["available"]
     assert usage_percentage == pytest.approx(100.0 * info["used"] / info["total"])
+
+    # clean up
+    shutil.rmtree(os.path.join(local_store[1]))
 
     return
