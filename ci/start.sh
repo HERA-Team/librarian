0c6105a144c939d99c3c532c62f7992e558f0b8b
Paul La Plante <plaplant@sas.upenn.edu>
2021-01-12 10:07:48 -0800

    Define docker-compose config that includes working store

--- ci/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# wait for postgres to be available
-./ci/wait-for-it.sh db:5432 -- alembic upgrade head
-runserver.py
---
+++ container/store_start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# put ssh config files in place
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+cat /run/secrets/id_rsa_pub > ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+# start sshd process
+echo "starting sshd process"
+/usr/sbin/sshd -D -o ListenAddress=0.0.0.0 -o PasswordAuthentication=no
