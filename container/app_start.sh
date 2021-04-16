5d3a46f519bc56c1a026cbf0c8ee4a925e694db7
Paul La Plante <plaplant@sas.upenn.edu>
2021-04-16 12:07:02 -0700

    Update app start file

--- container/app_start.sh
+++ container/app_start.sh
@@ -3,8 +3,8 @@
 # put ssh config files in place
 mkdir -p ~/.ssh
 cp container/ssh_config ~/.ssh/config
-cp /run/secrets/id_rsa_pub ~/.ssh/id_rsa.pub
-cp /run/secrets/id_rsa ~/.ssh/id_rsa
+cp /secrets/id_rsa_pub ~/.ssh/id_rsa.pub
+cp /secrets/id_rsa ~/.ssh/id_rsa
 
 # wait for postgres to be available
 ./container/wait-for-it.sh db:5432 -- alembic upgrade head
