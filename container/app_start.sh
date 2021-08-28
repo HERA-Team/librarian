08881f9309c07c4bb455758dc78c44f38813c298
Paul La Plante <plaplant@sas.upenn.edu>
2021-08-27 20:00:41 -0700

    Update how server config is specified in container

--- container/app_start.sh
+++ container/app_start.sh
@@ -6,6 +6,9 @@ cp container/ssh_config ~/.ssh/config
 cp /secrets/id_rsa_pub ~/.ssh/id_rsa.pub
 cp /secrets/id_rsa ~/.ssh/id_rsa
 
+# put server config in place
+cp /secrets/server-config.json /usr/src/app/server-config.json
+
 # wait for postgres to be available
 ./container/wait-for-it.sh db:5432 -- alembic upgrade head
 exec runserver.py
