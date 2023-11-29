fa512c36d2029a016290e65e2cbd92d0bacb7d2c
Josh Borrow <josh@joshborrow.com>
2023-11-29 14:16:23 -0500

    Change pathing to be local...

--- hera_librarian/stores/core.py
+++ hera_librarian/stores/core.py
@@ -2,12 +2,16 @@
 Core store (prototype).
 """
 
+from .pathinfo import PathInfo
+from pathlib import Path
+
+
 class CoreStore:
     """
     Prototype for store management. Should never be used directly
     (other than for type hints!). All derived classes must
     implement all of the functions defined in this prototype.
-    
+
     All functions should be executed 'on' the store. That may involve
     connecting to remote machines!
     """
@@ -16,14 +20,14 @@ class CoreStore:
 
     def __init__(self, name: str):
         self.name = name
-    
+
     @property
     def available(self) -> bool:
         """
         Is the store available?
         """
         raise NotImplementedError
-    
+
     @property
     def free_space(self) -> int:
         """
@@ -33,8 +37,8 @@ class CoreStore:
             Number of bytes available.
         """
         raise NotImplementedError
-    
-    def stage(self, file_size: int) -> str:
+
+    def stage(self, file_size: int) -> Path:
         """
         Creates space in the staging area for a file of size file_size.
 
@@ -46,23 +50,23 @@ class CoreStore:
         Returns
         -------
         str
-            Absolute path on the staging machine.
+            Relative path on the staging store. To get
         """
 
         raise NotImplementedError
-    
-    def unstage(self, path: str):
+
+    def unstage(self, path: Path):
         """
         Remove a file from the staging area.
 
         Parameters
         ----------
         path: str
-            Absolute path on the staging machine.
+            Relative path to the staging store.
         """
         raise NotImplementedError
-    
-    def commit(self, staging_path: str, store_path: str):
+
+    def commit(self, staging_path: Path, store_path: Path):
         """
         Commit a file from the staging area to the store.
 
@@ -81,11 +85,27 @@ class CoreStore:
         the store should be able to re-create itself from that dictionary.
         """
         raise NotImplementedError
-    
+
     @classmethod
     def from_dict(cls, d: dict) -> "CoreStore":
         """
         Creates a store from a dictionary. The dictionary should be
         the same as the one returned by to_dict.
+        """
+        raise NotImplementedError
+
+    def path_info(self, path: Path) -> PathInfo:
+        """
+        Get information about a file or directory at a path.
+
+        Parameters
+        ----------
+        path : Path
+            Path to do this at.
+
+        Returns
+        -------
+        PathInfo
+            Filled PathInfo object.
         """
         raise NotImplementedError
--- hera_librarian/stores/local.py
+++ hera_librarian/stores/local.py
@@ -4,12 +4,15 @@ all commands on the machine local to the librarian server.
 """
 
 from .core import CoreStore
+from .pathinfo import PathInfo
+from ..utils import get_md5_from_path, get_size_from_path, get_type_from_path
 
 from pathlib import Path
 
 import os
 import shutil
 import uuid
+
 
 class LocalStore(CoreStore):
     staging_path: Path
@@ -30,9 +33,28 @@ class LocalStore(CoreStore):
     def free_space(self) -> int:
         return min(
             shutil.disk_usage(self.store_path).free,
-            shutil.disk_usage(self.staging_path).free
+            shutil.disk_usage(self.staging_path).free,
         )
-    
+
+    def _resolved_path_staging(self, path: Path) -> Path:
+        complete_path = (self.staging_path / path).resolve()
+
+        # Check if the file is validly in our staging area. Someone
+        # could pass us ../../../../../../etc/passwd or something.
+
+        if not (self.staging_path.resolve() in complete_path.parents):
+            raise ValueError(f"Provided path {path} resolves outside staging area.")
+
+        return complete_path
+
+    def _resolved_path_store(self, path: Path) -> Path:
+        complete_path = (self.store_path / path).resolve()
+
+        if not (self.store_path.resolve() in complete_path.parents):
+            raise ValueError(f"Provided path {path} resolves outside store area.")
+
+        return complete_path
+
     def stage(self, file_size: int) -> Path:
         if file_size > self.free_space:
             raise ValueError("Not enough free space on store")
@@ -40,38 +62,53 @@ class LocalStore(CoreStore):
         # TODO: Do we want to actually keep track of files we have staged?
         #       Also maybe we want to check if the staging area is clear at startup?
 
-        stage_path = self.staging_path / f"{uuid.uuid4()}.tmp"
+        stage_path = Path(f"{uuid.uuid4()}.tmp")
 
         # Create the empty file.
-        stage_path.touch()
+        (self.staging_path / stage_path).touch()
 
         return stage_path
 
     def unstage(self, path: Path):
-        # TODO: Check if this path is within the staging area.
-        
-        if os.path.exists(path):
-            os.remove(path)
+        complete_path = self._resolved_path_staging(path)
+
+        if os.path.exists(complete_path):
+            os.remove(complete_path)
 
         return
-    
-    def commit(self, staging_path: Path, store_path: Path):
-        # TODO: Check if these paths are within the staging and store area.
 
-        shutil.move(staging_path, store_path)
-    
+    def commit(self, staging_path: Path, store_path: Path):
+        shutil.move(
+            self._resolved_path_staging(staging_path),
+            self._resolved_path_store(store_path),
+        )
 
     def to_dict(self):
         return {
             "name": self.name,
             "staging_path": str(self.staging_path),
-            "store_path": str(self.store_path)
+            "store_path": str(self.store_path),
         }
-    
+
     @classmethod
     def from_dict(cls, d):
         return cls(
             name=d["name"],
             staging_path=Path(d["staging_path"]),
-            store_path=Path(d["store_path"])
+            store_path=Path(d["store_path"]),
+        )
+
+    def path_info(self, path: Path) -> PathInfo:
+        # Promote path to object if required
+        path = Path(path)
+
+        if not path.exists():
+            raise FileNotFoundError(f"Path {path} does not exist")
+
+        return PathInfo(
+            # Use the old functions for consistency.
+            path=path,
+            filetype=get_type_from_path(str(path)),
+            md5=get_md5_from_path(path),
+            size=get_size_from_path(path),
         )
