bde923fd141aa8a72455d6613ba022762f4456d5
Josh Borrow <josh@joshborrow.com>
2023-11-29 09:48:58 -0500

    Added new required transfer manager info

--- hera_librarian/transfers/core.py
+++ hera_librarian/transfers/core.py
@@ -18,3 +18,24 @@ class CoreTransferManager:
             Path to store file at on destination host.
         """
         raise NotImplementedError
+
+    @property
+    def valid(self) -> bool:
+        """
+        Whether or not this transfer manager is valid for the
+        current system we are running on.
+        """
+        raise NotImplementedError
+
+    def to_dict(self):
+        """
+        Convert this transfer manager to a dictionary, for storage in the databsae.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        """
+        Create a transfer manager from a dictionary.
+        """
+        raise NotImplementedError
--- hera_librarian/transfers/local.py
+++ hera_librarian/transfers/local.py
@@ -4,13 +4,34 @@ Local transfer. Basically just a wrapper around `cp`.
 
 import shutil
 from pathlib import Path
+from socket import gethostname
 
 from .core import CoreTransferManager
 
 class LocalTransferManager(CoreTransferManager):
-    def __init__(self):
+    # The hostname of the machine being transferred to.
+    hostname: str 
+
+    def __init__(self, hostname: str):
         super().__init__()
 
+        self.hostname = hostname
+
+        return
+
     def transfer(self, local_path: Path, remote_path: Path):
+        # TODO: Verify that the location we are trying to copy to exists.
         return shutil.copy(local_path, remote_path)
+
+    @property
+    def valid(self) -> bool:
+        return gethostname() == self.hostname
     
+    def to_dict(self) -> dict:
+        return {
+            "hostname": self.hostname,
+        }
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "LocalTransferManager":
+        return cls(hostname=d["hostname"])
