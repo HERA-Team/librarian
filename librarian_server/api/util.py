53bd65cf104ace4ea846e878ca6511e0db2f3fba
Josh Borrow <josh@joshborrow.com>
2023-11-30 13:36:40 -0500

    Double decorate function in pydantic_api

--- librarian_server/api/util.py
+++ librarian_server/api/util.py
@@ -6,10 +6,11 @@ from pydantic import BaseModel
 
 from flask import request, jsonify, Response
 from typing import Optional
+from functools import wraps
 
 # TODO: Authentication
 
-def pydantic_api(f, recieve_model: Optional[BaseModel] = None):
+def pydantic_api(recieve_model: Optional[BaseModel] = None):
     """
     This decorator wraps API functions and serializes and deserializes
     them based upon the expected response types. 
@@ -18,50 +19,54 @@ def pydantic_api(f, recieve_model: Optional[BaseModel] = None):
     argument of 'request' is provided to the function that is the
     deserialized request body.
     """
+    def decorator(f):
+        def wrapped(*args, **kwargs):
+            # If we have a recieve model, we need to deserialize the
+            # request body into it.
+            if recieve_model is not None:
+                try:
+                    request_data = request.get_json()
+                except:
+                    return jsonify({
+                        "error": "Invalid JSON."
+                    }), 400
 
-    def wrapped(*args, **kwargs):
-        # If we have a recieve model, we need to deserialize the
-        # request body into it.
-        if recieve_model is not None:
+                try:
+                    request_model = recieve_model.model_validate_json(request_data)
+                except:
+                    return jsonify({
+                        "error": "Invalid request body."
+                    }), 400
+
+                kwargs["request"] = request_model
+
+            # Now run the function.
             try:
-                request_data = request.get_json()
-            except:
+                result = f(*args, **kwargs)
+            except Exception as e:
                 return jsonify({
-                    "error": "Invalid JSON."
-                }), 400
+                    "Internal server error": str(e)
+                }), 500
 
-            try:
-                request_model = recieve_model(**request_data)
-            except:
-                return jsonify({
-                    "error": "Invalid request body."
-                }), 400
+            # If the result is a Response, just return it.
+            if isinstance(result, Response):
+                return result
 
-            kwargs["request"] = request_model
+            # If the result is a tuple, assume it is (data, status).
+            if isinstance(result, tuple):
+                data, status = result
+            else:
+                data, status = result, 200
 
-        # Now run the function.
-        try:
-            result = f(*args, **kwargs)
-        except Exception as e:
-            return jsonify({
-                "error": str(e)
-            }), 500
+            # If the data is a pydantic model, serialize it.
+            if isinstance(data, BaseModel):
+                data = data.model_dump_json()
+            else:
+                # Just try to jsonify our stuff
+                data = jsonify(data)
 
-        # If the result is a Response, just return it.
-        if isinstance(result, Response):
-            return result
 
-        # If the result is a tuple, assume it is (data, status).
-        if isinstance(result, tuple):
-            data, status = result
-        else:
-            data, status = result, 200
-
-        # If the data is a pydantic model, serialize it.
-        if isinstance(data, BaseModel):
-            data = dict(data)
-
-        # Return the data.
-        return jsonify(data), status
-
-    return wrapped
+            # Return the data.
+            return data, status
+        return wrapped
+    return decorator
