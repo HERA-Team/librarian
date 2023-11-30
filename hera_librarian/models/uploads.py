90af80828e0683f1d620934e897b196071137806
Josh Borrow <josh@joshborrow.com>
2023-11-30 13:35:49 -0500

    Fix issue with pydantic subclassing

--- hera_librarian/models/uploads.py
+++ hera_librarian/models/uploads.py
@@ -2,12 +2,13 @@
 Models for uploads.
 """
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, SerializeAsAny
 
 from ..stores import CoreStore
-from ..transfers import CoreTransferManager
+from ..transfers import CoreTransferManager, LocalTransferManager
 
 from pathlib import Path
+from typing import Union
 
 class UploadInitiationRequest(BaseModel):
     """
@@ -26,7 +27,11 @@ class UploadInitiationResponse(BaseModel):
     available_bytes_on_store: int
     store_name: str
     staging_location: Path
-    transfer_providers: dict[str, CoreTransferManager]
+    staging_name: Path
+    # Note that transfer_providers will be tried in  the order specified here. So
+    # you should put the one that requires the most arguments _first_ (otherwise everything
+    # will come out as a CoreTransferManager!)
+    transfer_providers: dict[str, Union[LocalTransferManager, CoreTransferManager]]
 
 
 class UploadCompletionRequest(BaseModel):
@@ -38,9 +43,11 @@ class UploadCompletionRequest(BaseModel):
 
     store_name: str
     staging_location: Path
+    staging_name: Path
     destination_location: Path
     transfer_provider_name: str
-    transfer_provider: CoreTransferManager
+    # See note on ordering in UploadInitiationResponse.
+    transfer_provider: Union[LocalTransferManager, CoreTransferManager]
 
     meta_mode: str
     deletion_policy: str
