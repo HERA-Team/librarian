3913500da8213e7464e52c716aeee2f6d75a3973
Josh Borrow <josh@joshborrow.com>
2023-12-12 17:09:06 -0500

    Run all tasks on startup.

--- librarian_background/__init__.py
+++ librarian_background/__init__.py
@@ -17,9 +17,12 @@ from .core import SafeScheduler
 def background():
     scheduler = SafeScheduler()
     # Set scheduling...
-    scheduler.every(1).minutes.do(CheckIntegrity(name="check_integrity", store_name="local_store", age_in_days=7))
+    scheduler.every(12).hours.do(CheckIntegrity(name="check_integrity", store_name="local_store", age_in_days=7))
 
-    # ...and run it.
+    # ...and run it all on startup.
+    scheduler.run_all()
+
+    # ...begin scheduling operations.
     while True:
         try:
             scheduler.run_pending()
