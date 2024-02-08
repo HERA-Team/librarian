68412df06baff9f36c946a69f075a9ee7a1b4236
Josh Borrow <josh@joshborrow.com>
2024-02-08 13:59:58 -0500

    Change transfer manager to deal with file permissions

--- hera_librarian/transfers/local.py
+++ hera_librarian/transfers/local.py
@@ -2,6 +2,7 @@
 Local transfer. Basically just a wrapper around `cp`.
 """
 
+import os
 import shutil
 from pathlib import Path
 from socket import gethostname
@@ -14,12 +15,39 @@ class LocalTransferManager(CoreTransferManager):
     "The hostname(s) of the machine being transferred to."
 
     def transfer(self, local_path: Path, remote_path: Path):
-        # TODO: Verify that the location we are trying to copy to exists.
+        # Need to make sure that the the permissions are correctly
+        # set on all files and directories that we copy over.
+        # They should have rw-rw-r-- and rwxrwxr-x permissions.
+
+        def set_for_file(file: Path):
+            if file.is_dir():
+                os.chmod(
+                    file,
+                    0o775,
+                )
+            else:
+                os.chmod(file, 0o664)
+
+        copy_success = False
+
         if local_path.is_dir():
-            return shutil.copytree(local_path, remote_path)
+            copy_success = shutil.copytree(local_path, remote_path)
         else:
             # Copy2 copies more metadata.
-            return shutil.copy2(local_path, remote_path)
+            copy_success = shutil.copy2(local_path, remote_path)
+
+        if not copy_success:
+            raise ValueError(f"Could not copy {local_path} to {remote_path}")
+
+        # Set base permission
+        set_for_file(remote_path)
+
+        if remote_path.is_dir():
+            for root, dirs, files in os.walk(remote_path):
+                for x in dirs + files:
+                    set_for_file(Path(root) / x)
+
+        return copy_success
 
     @property
     def valid(self) -> bool:
