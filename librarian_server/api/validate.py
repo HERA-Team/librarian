452f4c4d4d92c58552a9d9d7dd5af84c0f7da65b
Josh Borrow <josh@joshborrow.com>
2024-11-11 12:08:43 -0500

    Use asyncer to dispatch verification tasks in parallel (#104)

--- librarian_server/api/validate.py
+++ librarian_server/api/validate.py
@@ -4,8 +4,11 @@ This can also have a 'chaining' effect, where the server will validate
 remote instances too.
 """
 
+import asyncio
+from pathlib import Path
 from time import perf_counter
 
+from asyncer import asyncify
 from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -30,10 +33,64 @@ from .auth import ReadonlyUserDependency
 router = APIRouter(prefix="/api/v2/validate")
 
 
+def calculate_checksum_of_local_copy(
+    original_checksum: str,
+    original_size: int,
+    path_info_function: callable,
+    path: Path,
+    store_id: int,
+    instance_id: int,
+):
+    start = perf_counter()
+    hash_function = get_hash_function_from_hash(original_checksum)
+    path_info = path_info_function(path, hash_function=hash_function)
+
+    response = FileValidationResponseItem(
+        librarian=server_settings.name,
+        store=store_id,
+        instance_id=instance_id,
+        original_checksum=original_checksum,
+        original_size=original_size,
+        current_checksum=path_info.checksum,
+        current_size=path_info.size,
+        computed_same_checksum=compare_checksums(original_checksum, path_info.checksum),
+    )
+    end = perf_counter()
+
+    log.debug(
+        f"Calculated path info for {instance_id} ({path_info.size} B) "
+        f"in {end - start:.2f} seconds."
+    )
+
+    return [response]
+
+
+def calculate_checksum_of_remote_copies(
+    librarian,
+    file_name,
+):
+    start = perf_counter()
+    try:
+        client = librarian.client()
+        responses = client.validate_file(file_name)
+    except (LibrarianHTTPError, LibrarianError):
+        log.error(
+            f"Failed to validate file {file_name} with librarian {librarian.name}"
+        )
+    end = perf_counter()
+
+    log.debug(
+        f"Validated file {file_name} with librarian {librarian.name} in {end - start:.2f} seconds."
+        f"Found {len(responses)} instances."
+    )
+
+    return responses
+
+
 @router.post(
     "/file", response_model=FileValidationResponse | FileValidationFailedResponse
 )
-def validate_file(
+async def validate_file(
     request: FileValidationRequest,
     response: Response,
     user: ReadonlyUserDependency,
@@ -74,50 +131,9 @@ def validate_file(
             suggested_remedy="Check the file name and try again.",
         )
 
-    checksum_info = []
-    # For each instance we need to calculate the path info.
-    for instance in file.instances:
-        if not instance.available:
-            continue
+    coroutines = []
 
-        start = perf_counter()
-        hash_function = get_hash_function_from_hash(file.checksum)
-        path_info = instance.store.store_manager.path_info(
-            instance.path, hash_function=hash_function
-        )
-        checksum_info.append(
-            FileValidationResponseItem(
-                librarian=server_settings.name,
-                store=instance.store.id,
-                instance_id=instance.id,
-                original_checksum=file.checksum,
-                original_size=file.size,
-                current_checksum=path_info.checksum,
-                current_size=path_info.size,
-                computed_same_checksum=compare_checksums(
-                    file.checksum, path_info.checksum
-                ),
-            )
-        )
-        end = perf_counter()
-
-        log.debug(
-            f"Calculated path info for {instance.id} ({path_info.size} B) "
-            f"in {end - start:.2f} seconds."
-        )
-
-        if not compare_checksums(file.checksum, path_info.checksum):
-            log_to_database(
-                severity=ErrorSeverity.CRITICAL,
-                category=ErrorCategory.DATA_INTEGRITY,
-                message=(
-                    "File validation failed The checksums do not match for "
-                    f"file {file.name} in store {instance.store.id}."
-                ),
-                session=session,
-            )
-
-    # Call up our neighbours and ask them!
+    # Call up our neighbours and ask them FIRST.
     # But what we actually have is a list of remote instances. There might
     # be more than one per librarian! First, use the list of remote instances
     # to generate a list of librarians we need to query.
@@ -138,22 +154,43 @@ def validate_file(
             continue
 
         # Now we can query the librarian for the file.
-        start = perf_counter()
-        try:
-            client = librarian.client()
-            response = client.validate_file(file.name)
-        except (LibrarianHTTPError, LibrarianError):
-            log.error(
-                f"Failed to validate file {file.name} with librarian {librarian.name}"
-            )
-            continue
-        end = perf_counter()
-
-        log.debug(
-            f"Validated file {file.name} with librarian {librarian.name} in {end - start:.2f} seconds."
-            f"Found {len(response)} instances."
+        responses = asyncify(calculate_checksum_of_remote_copies)(
+            librarian=librarian, file_name=request.file_name
         )
 
-        checksum_info += response
+        coroutines.append(responses)
+
+    # For each instance we need to calculate the path info.
+    for instance in file.instances:
+        if not instance.available:
+            continue
+
+        this_checksum_info = asyncify(calculate_checksum_of_local_copy)(
+            original_checksum=file.checksum,
+            original_size=file.size,
+            path_info_function=instance.store.store_manager.path_info,
+            path=instance.path,
+            store_id=instance.store.id,
+            instance_id=instance.id,
+        )
+
+        coroutines.append(this_checksum_info)
+
+    checksum_info = await asyncio.gather(*coroutines)
+
+    # Flatten checksum_info
+    checksum_info = [item for sublist in checksum_info for item in sublist]
+
+    for info in checksum_info:
+        if not info.computed_same_checksum and info.librarian == server_settings.name:
+            log_to_database(
+                severity=ErrorSeverity.CRITICAL,
+                category=ErrorCategory.DATA_INTEGRITY,
+                message=(
+                    "File validation failed The checksums do not match for "
+                    f"file {file.name} in store {instance.store.id}."
+                ),
+                session=session,
+            )
 
     return FileValidationResponse(checksum_info)
