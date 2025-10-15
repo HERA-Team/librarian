01dcb5b1948d14801ca00fb5913a435b0ae4a6da
Bryna Hazelton <bryna.hazelton@gmail.com>
2025-10-14 20:12:26 -0700

    try to fix missing observation record error

--- librarian_server/mc_integration.py
+++ librarian_server/mc_integration.py
@@ -20,7 +20,9 @@ note_file_upload_succeeded
 register_callbacks
 """.split()
 
+import sys
 import time
+
 from astropy.time import Time
 from sqlalchemy.engine.row import Row
 from sqlalchemy.exc import InvalidRequestError, SQLAlchemyError
@@ -86,14 +88,14 @@ class MCManager:
     def check_in(self):
         from sqlalchemy import func
 
-        from .file import File, FileInstance
-        from .store import Store
-
-        astro_now = Time.now()
-        unix_now = time.time()
-
-        # First, report our general status info.
         with app.app_context():
+            from .file import File, FileInstance
+            from .store import Store
+
+            astro_now = Time.now()
+            unix_now = time.time()
+
+            # First, report our general status info.
             num_files = db.session.query(func.count(File.name)).scalar() or 0
 
             data_volume_gb = (
@@ -103,16 +105,16 @@ class MCManager:
                 or 0
             ) / 1024**3
 
-        free_space_gb = 0
-        for store in Store.query.filter(Store.available):
-            free_space_gb += store.get_space_info()["available"]  # measured in bytes
-        free_space_gb /= 1024**3  # bytes => GiB
+            free_space_gb = 0
+            for store in Store.query.filter(Store.available):
+                free_space_gb += store.get_space_info()["available"]  # measured in bytes
+            free_space_gb /= 1024**3  # bytes => GiB
 
-        upload_min_elapsed = (unix_now - self._last_file_upload_time) / 60
+            upload_min_elapsed = (unix_now - self._last_file_upload_time) / 60
 
-        from .bgtasks import get_unfinished_task_count
+            from .bgtasks import get_unfinished_task_count
 
-        num_processes = get_unfinished_task_count()
+            num_processes = get_unfinished_task_count()
 
         try:
             self.mc_session.add_lib_status(
@@ -334,6 +336,12 @@ def create_observation_record(obsid):
         raise ServerError("expected M&C to know about obsid %s but it didn't", obsid)
 
     db.session.add(rec)
+    try:
+        db.session.commit()
+    except SQLAlchemyError:
+        db.session.rollback()
+        app.log_exception(sys.exc_info())
+        raise ServerError("failed to add new observation record %s to database; see logs for details", rec)
     return rec
 
 
